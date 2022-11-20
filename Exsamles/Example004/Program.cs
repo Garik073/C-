8d2b76fd7c1f28c2abbf6737c5dc5110aaa84469
Igor <gariklukin073@gmail.com>
2022-11-20 15:12:14 +0400

    Example004 Uslovie

--- Exsamles/Example004/Program.cs
+++ Exsamles/Example004/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Vvedite name polzovatela  пше");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
